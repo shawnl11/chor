2e8d41fc0dffa2cd3f6924b4f72fc0da33e30048
alex <abc1234@uw.edu>
2015-08-27 15:03:28 -0700

    added email settings to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,10 @@ Rails.application.configure do
     user_name: MANDRILL['username'],
     password: MANDRILL['password']
     }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
