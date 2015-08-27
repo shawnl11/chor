219ef9c2ffb141c04139690fa8ba00530f816433
alex <abc1234@uw.edu>
2015-08-26 21:58:41 -0700

    Chore Creation Spec

--- spec/requests/chore_create_spec.rb
+++ spec/requests/chore_create_spec.rb
@@ -11,7 +11,14 @@ describe "user sign in" do
     fill_in "user_password", :with => "12345678"
 
     click_button "Sign in"
-
-    expect(page).to have_content("A Chore Tracking Application")
+    visit "/chores/new"
+    expect(page).to have_content("Title")
+    
+    fill_in "chore_title", :with => "Vaccum the Duck"
+    fill_in "chore_url", :with => "url"
+    fill_in "chore_allday", :with => "true"
+  
+    click_button "Create Chore"
+    expect(page).to have_content("Vaccum the Duck")
   end
 end
