9881eb63b99d4a0fa103dc14aa5e1179bc999f2c
alex <abc1234@uw.edu>
2015-08-27 15:39:59 -0700

    experiment with callback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   #resources :chores
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # OAuth
   get 'auth/twitter/callback', to: 'sessions#twitter', as: :twitter_auth
   get 'auth/failure', to: 'sessions#failure'
-
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
